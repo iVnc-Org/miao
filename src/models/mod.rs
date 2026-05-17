7449cd712cbbbbfa30a01dcf3397e3a673cea136
root <root@git.com>
2026-05-17 08:26:21 +0000

    fix: export default socks port constant

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -5,7 +5,7 @@ pub mod proxy;
 pub mod version;
 
 pub use api::{ApiResponse, ConnectivityResult, StatusData, SubRequest, SubStatus};
-pub use config::{Config, DEFAULT_PORT};
+pub use config::{Config, DEFAULT_PORT, DEFAULT_SOCKS_PORT};
 pub use node::{AnyTls, DeleteNodeRequest, Hysteria2, NodeInfo, NodeRequest, Shadowsocks, Tls};
 pub use proxy::LastProxy;
 pub use version::{GitHubAsset, GitHubRelease, VersionInfo};
